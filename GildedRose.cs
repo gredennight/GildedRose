23282882af5585c18ff3817426cb299a75981293
Greden Night <greden.ua@gmail.com>
2023-04-23 12:52:48 +0300

    new UpdateQuality method, now works with Conjured items

--- GildedRose.cs
+++ GildedRose.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace csharp
 {
@@ -19,12 +20,101 @@ namespace csharp
             }
             return output;
         }
+        void UpdateItem(ref Item item,int sellInValueFactor,int qualityValueFactor)
+        {
+            item.SellIn     += sellInValueFactor;
+            item.Quality    = Math.Max(0, Math.Min(50, item.Quality + qualityValueFactor));
+
+        }
+        bool IsExpired(Item item)
+        {
+            bool output = true;
+            if(item.SellIn > 0) 
+                output = false;
+            return output;
+        }
+        Item GetNewAgedBrie(Item item)
+        {
+            if (IsExpired(item))
+                UpdateItem(ref item, -1, 2);
+            else
+                UpdateItem(ref item, -1, 1);
+            return item;
+        }
+        Item GetNewBackstagesPasses(Item item)
+        {
+            if (IsExpired(item))
+                item.Quality = 0;
+            else
+            {
+                if (item.SellIn <= 10 && item.SellIn > 5)
+                {
+                    UpdateItem(ref item, -1, 2);
+                }
+                else if (item.SellIn <= 5)
+                {
+                    UpdateItem(ref item, -1, 3);
+                }
+                else
+                {
+                    UpdateItem(ref item, -1, 1);
+                }
+            }
+            return item;
+        }
+        Item GetNewConjured(Item item)
+        {
+            if (IsExpired(item))
+            {
+                UpdateItem(ref item, -1, -4);
+            }
+            else
+            {
+                UpdateItem(ref item, -1, -2);
+            }
+            return item;
+        }
+        Item GetNewDefault(Item item)
+        {
+            if (IsExpired(item))
+            {
+                UpdateItem(ref item, -1, -2);
+            }
+            else
+            {
+                UpdateItem(ref item, -1, -1);
+            }
+            return item;
+        }
         public void UpdateQuality()
+        {
+            for(int itemIndex = 0; itemIndex < Items.Count; itemIndex++)
+            {
+                switch (Items[itemIndex].Name)
+                {
+                    case "Aged Brie":
+                        Items[itemIndex]=GetNewAgedBrie(Items[itemIndex]);
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        Items[itemIndex] = GetNewBackstagesPasses(Items[itemIndex]);
+                        break;
+                    case "Conjured Mana Cake":
+                        Items[itemIndex] = GetNewConjured(Items[itemIndex]);
+                        break;
+                    default:
+                        Items[itemIndex] = GetNewDefault(Items[itemIndex]);
+                        break;
+                }
+            }
+        }
+        private void UpdateQualityOld()
         {
             for (var i = 0; i < Items.Count; i++)
             {
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {//якщо це товар який не покращує свою якість з часом
+                {
                     if (Items[i].Quality > 0)
                     {
                         if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
@@ -34,7 +124,7 @@ namespace csharp
                     }
                 }
                 else
-                {//якщо він покращує свою якість
+                {
                     if (Items[i].Quality < 50)
                     {
                         Items[i].Quality = Items[i].Quality + 1;
